bd4465ba1a4696bc76b89ddab67e1fb62eff5707
Yash Thakur <yashthakur99755@gmail.com>
2025-10-05 21:39:09 +0530

    new

---
+++ index.js
@@ -0,0 +1,11 @@
+let loginbtn = document.getElementById("login");
+
+loginbtn.addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+let signupbtn = document.getElementById("signup");
+
+signupbtn.addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
